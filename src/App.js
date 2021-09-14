68e87aee1c93ce98b804b333d2f73c6ffe5fe699
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro.local>
2021-09-14 11:39:02 -0400

    know how to add scss to a react application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import MainPage from './components/MainPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Sass-React Practice</h1>
+      <Navbar />
+      <MainPage />
     </div>
   );
 }
---
+++ src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/MainPage.css'
+
+const MainPage = () => {
+    return (
+        <div className="main_page">
+            <h1>Welcome to the website!</h1>
+            <button>Click Me</button>
+        </div>
+    )
+}
+
+export default MainPage
